bcb9bbc4d5ee78585adf51c1d558d01f408786d4
desweb <audric.fourez@gmail.com>
2013-05-22 14:00:40 +0200

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForgeRor::Application.config.secret_token = '6718a4ea5fbcf719236c0ed2c399c193f89160a0fbcc879e8f543f98ec91a1bc5df24f12be3907df0ccbdf01b66a7b3fb0d7a9071a1565743b2704c7e8c0de0b'
