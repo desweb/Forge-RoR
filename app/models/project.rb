bb4ecdc61cbd102ebd9c357fe22d4635fe1549c0
desweb <audric.fourez@gmail.com>
2013-05-23 16:10:31 +0200

    fields require in model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,9 @@ class Project < ActiveRecord::Base
 	resourcify
 	attr_accessible :description, :state, :title
   
+  validates_presence_of :description
+  validates_presence_of :state
+  validates_presence_of :title
+  
   has_many :tickets
 end
