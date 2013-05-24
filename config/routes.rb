aa74cbf60d01645d52c3a5ae5fe58da16aed5acd
desweb <audric.fourez@gmail.com>
2013-05-24 14:55:53 +0200

    fix #2

--- config/routes.rb
+++ config/routes.rb
@@ -74,4 +74,6 @@ ForgeRor::Application.routes.draw do
     # Note: This route will make all actions in every controller accessible via GET requests.
     # match ':controller(/:action(/:id))(.:format)'
   end
+  
+  root :to => 'tickets#index'
 end
